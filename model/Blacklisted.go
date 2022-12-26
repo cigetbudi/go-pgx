5659b0604104d1237e9ed3ad7ea36be4edf85a94
stonear <27681405+stonear@users.noreply.github.com>
2022-12-26 11:52:05 +0700

    chore: remove unused

--- model/Blacklisted.go
+++ model/Blacklisted.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"context"
-	"fmt"
 	"go-pgx/database"
 )
 
@@ -12,7 +11,6 @@ type Blacklisted struct {
 }
 
 func AddBlacklisted(b *Blacklisted) error {
-	fmt.Println(database.DB)
 	_, err := database.DB.Exec(context.Background(), "insert into blacklisted(username, email) values ($1, $2)", b.Username, b.Email)
 	return err
 }
